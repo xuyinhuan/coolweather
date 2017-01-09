d0e7ab1a57932156606bd1c9e7e8aa595b14c5b4
xuyinhuan <xyhtech@foxmail.com>
2017-01-09 14:45:50 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/xyh/coolweather/MainActivity.java
+++ app/src/main/java/com/xyh/coolweather/MainActivity.java
@@ -1,7 +1,10 @@
 package com.xyh.coolweather;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+    }
+
+
+
 }
